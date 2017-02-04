79f9e7f2a60323c9504dd1b4ed3c5ed0fd569f19
DigiLabBarking <digilab@brightersteppings.co.uk>
2017-02-04 23:56:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 """
 Python Web Service
 """
-
 #!/usr/bin/env python
 import web
 from requests import Session
-File = open('data.txt','r')
-s = Session()
-for line in File:
-    resp = s.get('http://0.0.0.0:8080/classify/', params={'user': str(line)}, stream=True)
-    content=resp.content
-    print(content)
+
+url = "https://gateway.watsonplatform.net/natural-language-classifier/api/v1/classifiers/f5b42fx173-nlc-1507/classify"
+
+urls = (
+    '/classify/(.*)', 'clasify_string'
+)
+
+app = web.application(urls, globals())
+
+class clasify_string:
+    def GET(self, user):
+        print(self)
+        s = Session()
+        s.verify = False
+        s.auth = ('e214758a-a5f4-4571-9a07-cb233ddfbc96', 'DVKxopmo6FH7')
+        resp = s.get(url, params={'text': str(user)}, stream=True)
+        content=resp.content
+        return str(content.split('\n')[4].split('"')[3])
+
+if __name__ == "__main__":
+    app.run()
